165dec845e63345b9b39b6f6d9ccb2c50bd68d71
Hannah <HannahMarie-Wilson@live.nmit.ac.nz>
2020-07-25 13:46:25 +1200

    Intitial Commit

---
+++ Assets/Scripts/BunnyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyController : MonoBehaviour
+{
+    private Rigidbody2D myrigidbody;
+    public float BunnyJumpForce = 500f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myrigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Jump"))
+        {
+            myrigidbody.AddForce(transform.up * BunnyJumpForce);
+        }
+    }
+}
